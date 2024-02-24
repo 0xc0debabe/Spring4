e2826214628d4d1b654c5577300e4e644c53440c
0xc0debabe <kongminoo@naver.com>
2024-02-24 15:09:51 +0900

    240224_Spring

---
+++ src/main/java/jpabook/jpashop/InitDb.java
@@ -0,0 +1,38 @@
+package jpabook.jpashop;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.persistence.EntityManager;
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@RequiredArgsConstructor
+public class InitDb {
+
+    private final InitService initService;
+
+    @PostConstruct
+    public void init() {
+
+
+    }
+
+    @Component
+    @Transactional
+    @RequiredArgsConstructor
+    static class InitService {
+
+        private final EntityManager em;
+        public void dbInit1() {
+            Member member = new Member();
+            member.setName("userA");
+            member.setAddress(new Address("서울", "1", "1111"));
+            em.persist(member);
+
+        }
+    }
+
+}
